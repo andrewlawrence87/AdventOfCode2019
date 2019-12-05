4d9d719150849af6c752ddc4d3f2086573d7f268
andrew.lawrence87@gmail.com <andrew.lawrence87@gmail.com>
2019-12-05 17:21:55 +1100

    Completed day 4 solution

--- src/main/DayFour.py
+++ src/main/DayFour.py
@@ -1,22 +1,19 @@
 def matches_code(num):
     res = list(map(int, str(num)))
+    match_code = False;
     for i in range(0, res.__len__() - 1):
-        print(res[i])
         if res[i] > res[i + 1]:
             return False
         if res[i] == res[i + 1]:
-            if i == 0 | res[i] != res[i - 1]:
-                if i + 2 > (res.__len__() - 1) | res[i + 1] != res[i + 2]:
-                    return True
+            if i == 0 or (res[i] != res[i - 1]):
+                if (i + 2 > (res.__len__() - 1)) or (res[i + 1] != res[i + 2]):
+                    match_code = True
+    if match_code:
+        return True
     return False
 
-
-print('Test 1 ' + str(matches_code(112233)))
-print('Test 2 ' + str(matches_code(123444)))
-print('Test 3 ' + str(matches_code(111122)))
-
-# k = 0
-# for i in range(382345, 843167):
-#     if matches_code(i):
-#         k += 1
-# print(k)
+k = 0
+for i in range(382345, 843167):
+    if matches_code(i):
+        k += 1
+print(k)
